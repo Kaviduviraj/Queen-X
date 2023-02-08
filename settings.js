e5ac077152fbcf77e3d5194d6fe7fa56d0b77602
AHINSA (BUBU) <118949057+Kaviduviraj@users.noreply.github.com>
2023-02-08 18:03:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,17 +15,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
-global.ownername = "©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
+global.owner = ['16812354058'] //ur owner number
+global.ownername = "🌼❤️Roshel❤️🌼" //ur owner name
 global.ytname = "ʏᴜʀᴇꜱʜ" //ur yt chanel name
 global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
-global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
+global.ownernomer = "16812354058" //ur number
+global.premium = ['16812354058'] //ur premium number
+global.botname = '🌼❤️Roshel❤️🌼' //ur bot name
+global.linkz = "https://chat.whatsapp.com/ELPmVmcoFF09ofwmBJKttg" //your theme url which will be displayed on whatsapp
 global.websitex = "https:// no" //ur website to be displayed
 global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
 global.themeemoji = "🛸" //ur theme emoji
